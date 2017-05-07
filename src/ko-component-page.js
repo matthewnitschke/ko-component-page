c05b3c362cca34510b1f6ab011be2268b8f78782
Matthew Nitschke <mathuc33@gmail.com>
2017-05-06 22:43:12 -0600

    Removed es6 dependencies, bundled prism

--- src/ko-component-page.js
+++ src/ko-component-page.js
@@ -42,17 +42,15 @@ ko.components.register("ko-component-page", {
       return self;
     }
   },
-  template: `
-  <div class='component-name' data-bind='text: componentName'></div>
-  <div class='component-page'>
-
-    <div class='center' style='margin: 2rem;' id="tabs">
-      <div class='component-tab-button selected' data-bind='click: parametersTabClicked'>Parameters</div>
-      <div class='component-tab-button' data-bind='click: examplesTabClicked'>Examples</div>
-    </div>
-    <!-- ko template: { nodes: $componentTemplateNodes } --><!-- /ko -->
-  </div>
-  `
+  template: "
+  <div class='component-name' data-bind='text: componentName'></div>\
+  <div class='component-page'>\
+    <div class='center' style='margin: 2rem;' id='tabs'>\
+      <div class='component-tab-button selected' data-bind='click: parametersTabClicked'>Parameters</div>\
+      <div class='component-tab-button' data-bind='click: examplesTabClicked'>Examples</div>\
+    </div>\
+    <!-- ko template: { nodes: $componentTemplateNodes } --><!-- /ko -->\
+  </div>"
 });
 
 ko.components.register("ko-component-parameters", {
@@ -78,19 +76,16 @@ ko.components.register("ko-component-parameters", {
       return self;
     }
   },
-  template: `
-  <div class='component-tab-title'>Parameters</div>
-
-  <div data-bind="visible: requiredParameters.length > 0">
-    <div class='component-parameters-group-label'>required</div>
-    <div class='component-parameters-group' data-bind="template: { nodes: requiredParameters }"></div>
-  </div>
-
-  <div data-bind="visible: optionalParameters.length > 0">
-    <div class='component-parameters-group-label'>optional</div>
-    <div class='component-parameters-group' data-bind='template: { nodes: optionalParameters }'></div>
-  </div>
-  `
+  template: "
+  <div class='component-tab-title'>Parameters</div>\
+  <div data-bind='visible: requiredParameters.length > 0'>\
+    <div class='component-parameters-group-label'>required</div>\
+    <div class='component-parameters-group' data-bind='template: { nodes: requiredParameters }'></div>\
+  </div>\
+  <div data-bind='visible: optionalParameters.length > 0'>\
+    <div class='component-parameters-group-label'>optional</div>\
+    <div class='component-parameters-group' data-bind='template: { nodes: optionalParameters }'></div>\
+  </div>"
 });
 
 ko.components.register("ko-component-parameter", {
@@ -100,25 +95,23 @@ ko.components.register("ko-component-parameter", {
     this.type = params.type;
     this.description = params.description;
   },
-  template: `
-  <div class='component-parameter-item'>
-    <div class='component-parameter-name' data-bind='text: name'></div>
-    <div class='component-parameter-info'>
-      <span data-bind='text: type' class='component-parameter-type'></span>
-      <div data-bind='text: description' class='component-parameter-description'></div>
-    </div>
-  </div>
-  `
+  template: "
+  <div class='component-parameter-item'>\
+    <div class='component-parameter-name' data-bind='text: name'></div>\
+    <div class='component-parameter-info'>\
+      <span data-bind='text: type' class='component-parameter-type'></span>\
+      <div data-bind='text: description' class='component-parameter-description'></div>\
+    </div>\
+  </div>"
 })
 
 ko.components.register("ko-component-examples",{
   viewModel: function(){},
-  template: `
-  <div>
-    <div class='component-tab-title'>Examples</div>
-    <!-- ko template: { nodes: $componentTemplateNodes } --><!-- /ko -->
-  </div>
-  `
+  template: "
+  <div>\
+    <div class='component-tab-title'>Examples</div>\
+    <!-- ko template: { nodes: $componentTemplateNodes } --><!-- /ko -->\
+  </div>"
 })
 
 ko.components.register("ko-component-example", {
@@ -151,19 +144,18 @@ ko.components.register("ko-component-example", {
       };
     }
   },
-  template: `
-  <div class='component-page-wrapper'>
-    <div class='component-label' data-bind='text: label'></div>
-    <div class='component-description' data-bind='text: description'></div>
-    <div class='component-wrapper' data-bind="with: vm">
-      <div class='component-example-wrapper' data-bind='template: { nodes: $componentTemplateNodes }'></div>
-      <div class='component-markup-header'>html</div>
-      <pre><code class='language-markup' data-bind="text: html"></code></pre>
-      <div style='margin-top:1rem' data-bind='visible: js.length > 0' class='component-markup-header'>js</div>
-      <pre data-bind='visible: js.length > 0'><code class='language-javascript' data-bind="text: js"></code></pre>
-    </div>
-  </div>
-  `
+  template: "
+  <div class='component-page-wrapper'>\
+    <div class='component-label' data-bind='text: label'></div>\
+    <div class='component-description' data-bind='text: description'></div>\
+    <div class='component-wrapper' data-bind='with: vm'>\
+      <div class='component-example-wrapper' data-bind='template: { nodes: $componentTemplateNodes }'></div>\
+      <div class='component-markup-header'>html</div>\
+      <pre><code class='language-markup' data-bind='text: html'></code></pre>\
+      <div style='margin-top:1rem' data-bind='visible: js.length > 0' class='component-markup-header'>js</div>\
+      <pre data-bind='visible: js.length > 0'><code class='language-javascript' data-bind='text: js'></code></pre>\
+    </div>\
+  </div>"
 });
 
 ko.components.register("ko-component-example-script", {
