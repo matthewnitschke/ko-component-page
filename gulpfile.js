cc104d57b9dbc2f9cfa4b050d532592ee9338a65
Matthew Nitschke <mathuc33@gmail.com>
2017-05-06 22:17:24 -0600

    Changes to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,19 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var strip = require('gulp-strip-comments');
 
-gulp.task('scripts', function() {
-    gulp.src(['src/ko-component-page.js', 'src/lib/prism.js'])
-      .pipe(concat('ko-component-page.js'))
-      .pipe(gulp.dest('dist'));
+// gulp.task('scripts', function() {
+//     gulp.src(['src/ko-component-page.js', 'src/lib/prism.js'])
+//       .pipe(concat('ko-component-page.js'))
+//       .pipe(gulp.dest('dist'));
+//
+//     gulp.src(['src/ko-component-page.css', 'src/lib/prism.css'])
+//       .pipe(concat('ko-component-page.css'))
+//       .pipe(gulp.dest('dist'));
+// });
 
-    gulp.src(['src/ko-component-page.css', 'src/lib/prism.css'])
-      .pipe(concat('ko-component-page.css'))
-      .pipe(gulp.dest('dist'));
-});
+gulp.task('scripts', function(){
+  gulp.src(['src/ko-component-page.html', 'src/ko-component-page.js', 'src/ko-component-page.css', 'src/lib/prism.js', 'src/lib/prism.css'])
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('default', ['scripts']);
